d613edf30319d304f3f4142eb6bd9f12d890c731
chashao <melvin.huang@berkeley.edu>
2016-06-20 13:24:12 -0700

    abstraction

--- diff_csv.py
+++ diff_csv.py
@@ -2,11 +2,12 @@
 #change the file names as needed for the files to be compared. Leave the rest alone.
 import csv
 
-
-with open("diff_1.csv", "rb") as orig_file:
-	with open("diff_2.csv", "rb") as new_file:
-		#with open("difffile.csv", "w+") as diff_file:
-		#uncomment this if you want to output the result to a new CSV
+#takes in filenames as strings
+def file_compare(first_file, second_file):
+	with open(first_file, "rb") as orig_file:
+		with open(second_file, "rb") as new_file:
+			#with open("difffile.csv", "w+") as diff_file:
+			#uncomment this if you want to output the result to a new CSV
 			reader_1 = csv.reader(orig_file)
 			reader_2 = csv.reader(new_file)
 			indexed_reader_2 = list(reader_2)
@@ -22,3 +23,5 @@ with open("diff_1.csv", "rb") as orig_file:
 					counter += 1
 				else:
 					counter += 1
+
+file_compare(first_file, second_file)
